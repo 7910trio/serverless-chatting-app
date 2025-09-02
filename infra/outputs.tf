5ff1c85feb73fe8a7312dabaed6832d3b6199bb0
7910trio <7910trio@naver.com>
2025-09-02 10:56:44 +0900

    Modify CICD YAML & Add root outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
 output "ws_url" {
   value = module.api_gateway.websocket_api_url
 }
+
+output "bucket_name" {
+  value = module.s3_website.bucket_name
+}
+
+output "cloudfront_id" {
+  value = module.cloudfront.cloudfront_id
+}
